55f6f69d829b673bf88dd6525ca9e58b0095dd0b
vandong <trinhdomg114@gmail.com>
2025-04-06 09:53:09 +0700

    sua port and add dist

--- dist/config/db.js
+++ dist/config/db.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const sequelize_1 = require("sequelize");
+const fs_1 = __importDefault(require("fs"));
 dotenv_1.default.config({ path: './.env' });
 const db = {
     host: process.env.DB_HOST || "localhost",
@@ -15,12 +16,12 @@ const db = {
 const sequelize = new sequelize_1.Sequelize(db.database, db.user, db.password, {
     host: db.host,
     dialect: 'mysql',
-    // port:21954,
-    // dialectOptions: {
-    //     ssl: {
-    //       ca: fs.readFileSync('../ca.pem'),
-    //       rejectUnauthorized: true,
-    //     },
-    //   },
+    port: 21954,
+    dialectOptions: {
+        ssl: {
+            ca: fs_1.default.readFileSync('../ca.pem'),
+            rejectUnauthorized: true,
+        },
+    },
 });
 exports.default = sequelize;
